9cbec8f592b3021aa1472e19fa0890557c16eb73
jake222333 <59941620+jake222333@users.noreply.github.com>
2020-02-07 11:04:13 -0800

    asdas
    dasdasd

--- des157-wi20/JS/script.js
+++ des157-wi20/JS/script.js
@@ -1,17 +1,37 @@
+var xPosition=100;  
+var yPosition=100; 
+var xSpeed=10;
+
 function setup() {
-    var myCanvas = createCanvas (600, 250);
-    frameRate(8);
+    var myCanvas = createCanvas (600,250);
+    frameRate(60);
     noStroke();
     background(random(255), random(255), random(255));
     myCanvas.parent(mySketch);
   }
   
- function draw() {
-    if( mouseIsPressed) {
-      fill(0);
-    } else {
-      fill(random(255), random(255), random(255), random(255));
-    }
-      var circSize = random(200);
-    ellipse(mouseX, mouseY, circSize, circSize);
+  function draw() { 
+    background (0); noStroke(); fill(255);
+  if(xPosition<600 )  { 
+    ellipse(xPosition, yPosition, 25, 25); 
+    xPosition=xPosition+xSpeed;
   }
+  if(xPosition>500 ){ 
+    yPosition=yPosition+xSpeed; 
+    xPosition=500;
+  }
+  if(yPosition>500){ 
+    xSpeed=-xSpeed; 
+    xPosition=xPosition+xSpeed; 
+    yPosition=500;
+  }
+  if(xPosition<100 ){ 
+    yPosition=yPosition+xSpeed; 
+    xPosition=100;
+  }
+  
+  if(yPosition<100) {
+    xSpeed=-xSpeed;
+    xPosition=xPosition+xSpeed; 
+    yPosition=101;
+  }}
